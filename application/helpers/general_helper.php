f57b1d8b7969d5d3dec5b1ce14a10ad32a2d2732
German Donoso <germanedt@gmail.com>
2022-02-10 15:31:39 -0500

    Cambios alertas

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -8,4 +8,29 @@ if(!function_exists('json_response'))
     }
 
 }
+
+if(!function_exists('calcularEdad'))
+{
+    function calcularEdad($fecha_nac) {
+        $fecha_nacimiento = new DateTime($fecha_nac);
+        $hoy = new DateTime();
+        return $hoy->diff($fecha_nacimiento)->y;
+    }
+
+}
+if(!function_exists('addRequestAdit'))
+{
+    function addRequestAdit($paciente, $tipo) {
+        $CI = &get_instance();
+        $CI->load->model('Auditoria_Model');
+        $data = array(
+            'id_usuario' => logged_user()['id_usuario'],
+            'id_paciente' => $paciente,
+            'fecha_auditoria' =>  date('Y-m-d'),
+            'tipo' => $tipo
+        );
+        return $CI->Auditoria_Model->create($data);
+    }
+}
+
 ?>
